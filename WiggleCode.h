1d31465a7f89410f3f419f361601218dace09488
Hydromancer <Hydromancer8@gmail.com>
2017-01-25 17:24:35 -0800

    Create WiggleCode.h

---
+++ WiggleCode.h
@@ -0,0 +1,15 @@
+/*
+ * WiggleCode.h
+ *
+ *  Created on: Feb 15, 2016
+ *      Author: Robot
+ */
+
+#ifndef WIGGLECODE_H_
+#define WIGGLECODE_H_
+
+float TargetAcquired(long double error, double p, double d);
+
+
+
+#endif /* WIGGLECODE_H_ */
